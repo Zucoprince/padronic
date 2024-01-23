13c4d3bf489757cfab0b95f9cbe440bc0fa24afd
Pedro Zucolo <pedro.zucolo@outlook.com>
2024-01-23 14:23:09 -0300

    Almost there.

--- src/PadronicServiceProvider.php
+++ src/PadronicServiceProvider.php
@@ -13,14 +13,6 @@ class PadronicServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $file = base_path('config/app.php');
-        $contents = file_get_contents($file);
-        $provider = 'Zucoprince\Padronic\PadronicServiceProvider::class,';
-
-        $put = str_replace("'providers' => ServiceProvider::defaultProviders()->merge([", "'providers' => ServiceProvider::defaultProviders()->merge([\n        $provider", $contents);
-
-        file_put_contents($file, $put);
-
         $this->publishes([
             __DIR__ . '/Commands' => app_path('Console/Commands'),
         ], 'padronic-commands');
