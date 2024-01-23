7d12072810386e9367e4999979232aea39df3114
Pedro Zucolo <pedro.zucolo@outlook.com>
2024-01-23 13:43:50 -0300

    Almost there.

--- src/PadronicServiceProvider.php
+++ src/PadronicServiceProvider.php
@@ -8,14 +8,27 @@ class PadronicServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->register(PadronicSetupServiceProvider::class);
+        //
     }
 
     public function boot()
     {
+        $this->addProviderToConfig();
+
         $this->publishes([
             __DIR__ . '/Commands' => app_path('Console/Commands'),
         ], 'padronic-commands');
+    }
+
+    protected function addProviderToConfig()
+    {
+        $file = base_path('config/app.php');
+        $contents = file_get_contents($file);
+        $provider = 'Zucoprince\Padronic\PadronicServiceProvider::class,';
+
+        $put = str_replace("'providers' => ServiceProvider::defaultProviders()->merge([", "'providers' => ServiceProvider::defaultProviders()->merge([\n        $provider", $contents);
+
+        file_put_contents($file, $put);
     }
 
 }
