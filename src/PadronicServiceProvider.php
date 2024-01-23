349d6ece0f3ad4bd29826afcb89c36fdae9ac3ce
Pedro Zucolo <pedro.zucolo@outlook.com>
2024-01-23 10:50:48 -0300

    Some Ajusts

---
+++ src/PadronicServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zucoprince\Padronic;
+
+use Illuminate\Support\ServiceProvider;
+
+class PadronicServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Aqui você pode fazer vinculações no contêiner de serviços, se necessário.
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/Commands' => app_path('app/Console/Commands'),
+        ], 'padronic-commands');
+    }
+
+}
