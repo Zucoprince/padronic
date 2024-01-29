80b3b1e8516b4be70d4df974efc43159b34c2be0
Pedro Zucolo <pedro.zucolo@outlook.com>
2024-01-29 15:39:25 -0300

    V0.0.2 trouxe melhorias na geração de arquivos e tratamento de if's;

--- src/Commands/All.php
+++ src/Commands/All.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zucoprince\Padronic\Commands;
+namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
@@ -50,7 +50,16 @@ class All extends Command
 
         $this->routeApi();
 
-        $this->info('Comando all foi executado com sucesso!');
+        $this->info('
+         ______________________________________________
+        |                                              |
+        |                                              |
+        |    Comando all foi executado com sucesso!    |
+        |                                              |
+        |______________________________________________|
+
+'
+        );
     }
 
     public function routeApi()
@@ -64,13 +73,19 @@ class All extends Command
         foreach (glob(\$dir) as \$file) {
             include \$file;
         }
-
         ";
 
-        file_put_contents($filePath, '');
-        File::append($filePath, $apiDefault);
+        $content = File::get($filePath);
+        $especificContent = "$" . "dir = __DIR__ . '/api/*';";
 
-        return $this->info("O arquivo $filePath foi alterado com sucesso!");
+        if (strpos($content, $especificContent) === false) {
+            file_put_contents($filePath, '');
+            File::append($filePath, $apiDefault);
+
+            return $this->info("O arquivo $filePath foi alterado com sucesso!");
+        } else {
+            return $this->alert("O arquivo $filePath já se encontra no formato certo");
+        }
     }
 
     public function incrementRepository($fileName)
@@ -85,16 +100,20 @@ class All extends Command
         $repositoryFilePath = $repositoriesPath . DIRECTORY_SEPARATOR . $fileName . 'Repository.php';
 
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            !File::exists($repositoryFilePath) ? exec("echo. > $repositoryFilePath") : $this->error("O arquivo $repositoryFilePath já existe no contexto atual.");
+            !File::exists($repositoryFilePath) && exec("echo. > $repositoryFilePath");
         } else {
-            !File::exists($repositoryFilePath) ? exec("touch $repositoryFilePath") : $this->error("O arquivo $repositoryFilePath já existe no contexto atual.");
+            !File::exists($repositoryFilePath) && exec("touch $repositoryFilePath");
         }
 
-        $repositoryDefault = $this->repositoryTxt($fileName, $fileNameLowerVar);
 
-        File::append($repositoryFilePath, $repositoryDefault);
+        if (!File::exists($repositoryFilePath)) {
+            $repositoryDefault = $this->repositoryTxt($fileName, $fileNameLowerVar);
+            File::append($repositoryFilePath, $repositoryDefault);
 
-        return $this->info("O arquivo $repositoryFilePath criado com sucesso!");
+            return $this->info("O arquivo $repositoryFilePath criado com sucesso!");
+        } else {
+            return $this->alert("O arquivo $repositoryFilePath já existe no contexto atual.");
+        }
     }
 
     public function incrementController($fileName)
@@ -102,11 +121,21 @@ class All extends Command
         $controllerPath = base_path('app/Http/Controllers');
         $controllerFilePath = $controllerPath . DIRECTORY_SEPARATOR . $fileName . 'Controller.php';
         $controllerDefault = $this->controllerTxt($fileName);
+        $validResponses = ['S', 'N', 'SIM', 'NAO', 'NÃO'];
+        $response = [];
 
-        file_put_contents($controllerFilePath, '');
-        File::append($controllerFilePath, $controllerDefault);
+        while (!in_array($response, $validResponses)) {
+            $response = strtoupper($this->ask("Gostaria de reescrever o arquivo $fileName" . "Controller.php para o padrão do Padronic? - [s/n]"));
+        }
 
-        return $this->info("O arquivo $controllerFilePath foi alterado com sucesso!");
+        if (in_array($response, ['S', 'SIM'])) {
+            file_put_contents($controllerFilePath, '');
+            File::append($controllerFilePath, $controllerDefault);
+
+            return $this->info("O arquivo $controllerFilePath foi alterado com sucesso!");
+        } else {
+            return $this->alert("O arquivo $controllerFilePath não foi alterado");
+        }
     }
 
     public function incrementRoute($fileName)
@@ -121,235 +150,233 @@ class All extends Command
         $routeFilePath = $routesPath . DIRECTORY_SEPARATOR . $fileName . '.php';
 
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            !File::exists($routeFilePath) ? exec("echo. > $routeFilePath") : $this->error("O arquivo $routeFilePath já existe no contexto atual.");
+            !File::exists($routeFilePath) && exec("echo. > $routeFilePath");
         } else {
-            !File::exists($routeFilePath) ? exec("touch $routeFilePath") : $this->error("O arquivo $routeFilePath já existe no contexto atual.");
+            !File::exists($routeFilePath) && exec("touch $routeFilePath");
         }
 
-        $routeDefault = $this->routeTxt($fileName, $fileNameLower);
 
-        File::append($routeFilePath, $routeDefault);
+        if (!File::exists($routeFilePath)) {
+            $routeDefault = $this->routeTxt($fileName, $fileNameLower);
+            File::append($routeFilePath, $routeDefault);
 
-        return $this->info("O arquivo $routeFilePath foi criado com sucesso!");
+            return $this->info("O arquivo $routeFilePath foi criado com sucesso!");
+        } else {
+            return $this->alert("O arquivo $routeFilePath já existe no contexto atual.");
+        }
     }
 
     public function repositoryTxt($fileName, $fileNameLowerVar)
     {
-        return "
-    <?php
+        return "<?php
 
-    namespace App\Http\Repositories;
+namespace App\Http\Repositories;
 
-    use App\Models\\$fileName;
-    use Exception;
-    use Illuminate\Pagination\LengthAwarePaginator;
-    use Illuminate\Support\Facades\DB;
-    use Illuminate\Support\Facades\Log;
+use App\Models\\$fileName;
+use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
-    class {$fileName}Repository
+class {$fileName}Repository
+{
+    protected \$model;
+
+    public function __construct($fileName \$model)
     {
-        protected \$model;
+        \$this->model = \$model;
+    }
 
-        public function __construct($fileName \$model)
-        {
-            \$this->model = \$model;
-        }
-
-        public function query(\$model, \$request): $fileName
-        {
-            foreach (\$request as \$key => \$value) {
-                switch (\$key) {
+    public function query(\$model, \$request): $fileName
+    {
+        foreach (\$request as \$key => \$value) {
+            switch (\$key) {
                 // Faça todos os querys necessarios aqui.
                 default:
                     break;
-                }
             }
-
-            return \$model;
         }
 
-        public function getAll(): LengthAwarePaginator
-        {
-            $fileNameLowerVar = \$this->model->orderBy('id');
-            \$query = \$this->query($fileNameLowerVar, request()->all());
+        return \$model;
+    }
 
-            return \$query->paginate(10);
-        }
+    public function getAll(): LengthAwarePaginator
+    {
+        $fileNameLowerVar = \$this->model->orderBy('id');
+        \$query = \$this->query($fileNameLowerVar, request()->all());
 
-        public function getById(int \$id): $fileName
-        {
-            $fileNameLowerVar = \$this->model->findOrFail(\$id);
+        return \$query->paginate(10);
+    }
+
+    public function getById(int \$id): $fileName
+    {
+        $fileNameLowerVar = \$this->model->findOrFail(\$id);
+
+        return $fileNameLowerVar;
+    }
+
+    public function create(array \$data): $fileName|Exception
+    {
+        try {
+            DB::beginTransaction();
+
+            $fileNameLowerVar = \$this->model->create(\$data);
+
+            DB::commit();
 
             return $fileNameLowerVar;
+        } catch (Exception \$e) {
+            DB::rollBack();
+            Log::error(\$e->getMessage());
+
+            throw \$e;
         }
-
-        public function create(array \$data): $fileName|Exception
-        {
-            try {
-                DB::beginTransaction();
-
-                $fileNameLowerVar = \$this->model->create(\$data);
-
-                DB::commit();
-
-                return $fileNameLowerVar;
-            } catch (Exception \$e) {
-                DB::rollBack();
-                Log::error(\$e->getMessage());
-
-                throw \$e;
-            }
-        }
-
-        public function update(int \$id, array \$data): $fileName|Exception
-        {
-            try {
-                DB::beginTransaction();
-
-                $fileNameLowerVar = \$this->model->findOrFail(\$id);
-
-                {$fileNameLowerVar}->fill(\$data);
-                {$fileNameLowerVar}->save();
-
-                DB::commit();
-
-                return $fileNameLowerVar;
-            } catch (Exception \$e) {
-                DB::rollBack();
-                Log::error(\$e->getMessage());
-
-                throw \$e;
-            }
-        }
-
-        public function delete(int \$id): String|Exception
-        {
-            try {
-                DB::beginTransaction();
-
-                $fileNameLowerVar = \$this->model->findOrFail(\$id);
-                \$id = {$fileNameLowerVar}->id;
-
-                {$fileNameLowerVar}->delete();
-
-                DB::commit();
-
-                return 'O $fileName ' . \$id . ' foi excluído com sucesso!';
-            } catch (Exception \$e) {
-                DB::rollBack();
-                Log::error(\$e->getMessage());
-
-                throw \$e;
-            }
-        }
-
     }
-    ";
+
+    public function update(int \$id, array \$data): $fileName|Exception
+    {
+        try {
+            DB::beginTransaction();
+
+            $fileNameLowerVar = \$this->model->findOrFail(\$id);
+
+            {$fileNameLowerVar}->fill(\$data);
+            {$fileNameLowerVar}->save();
+
+            DB::commit();
+
+            return $fileNameLowerVar;
+        } catch (Exception \$e) {
+            DB::rollBack();
+            Log::error(\$e->getMessage());
+
+            throw \$e;
+        }
+    }
+
+    public function delete(int \$id): String|Exception
+    {
+        try {
+            DB::beginTransaction();
+
+            $fileNameLowerVar = \$this->model->findOrFail(\$id);
+            \$id = {$fileNameLowerVar}->id;
+
+            {$fileNameLowerVar}->delete();
+
+            DB::commit();
+
+            return 'O $fileName ' . \$id . ' foi excluído com sucesso!';
+        } catch (Exception \$e) {
+            DB::rollBack();
+            Log::error(\$e->getMessage());
+
+            throw \$e;
+        }
+    }
+
+}";
     }
 
     public function controllerTxt($fileName)
     {
-        return "
-        <?php
+        return "<?php
 
-        namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-        use App\Http\Repositories\\{$fileName}Repository;
-        use App\Http\Requests\\{$fileName}Request;
-        use App\Http\Resources\\{$fileName}Resource;
-        use App\Traits\ApiResponser;
-        use Illuminate\Http\JsonResponse;
-        use Illuminate\Http\Response;
+use App\Http\Repositories\\{$fileName}Repository;
+use App\Http\Requests\\{$fileName}Request;
+use App\Http\Resources\\{$fileName}Resource;
+use App\Traits\ApiResponser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
-        class {$fileName}Controller extends Controller
-        {
-            use ApiResponser;
+class {$fileName}Controller extends Controller
+{
+    use ApiResponser;
 
-            protected \$repository;
+    protected \$repository;
 
-            public function __construct({$fileName}Repository \$repository)
-            {
-                \$this->repository = \$repository;
-            }
+    public function __construct({$fileName}Repository \$repository)
+    {
+        \$this->repository = \$repository;
+    }
 
-            public function callGetAll(): JsonResponse
-            {
-                \$data = \$this->repository->getAll();
+    public function callGetAll(): JsonResponse
+    {
+        \$data = \$this->repository->getAll();
+        empty(\$data) || \$data === null ?
+            \$response = \$this->errorResponse('Não foi possível trazer nada de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
+            \$response = \$this->successResponse({$fileName}Resource::collection(\$data)->response()->getData());
 
-                empty(\$data) || \$data === null ?
-                    \$response = \$this->errorResponse('Não foi possível trazer nada de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
-                    \$response = \$this->successResponse({$fileName}Resource::collection(\$data)->response()->getData());
+        return \$response;
+    }
 
-                return \$response;
-            }
+    public function callCreate({$fileName}Request \$request): JsonResponse
+    {
+        \$data = \$this->repository->create(\$request->all());
 
-            public function callCreate({$fileName}Request \$request): JsonResponse
-            {
-                \$data = \$this->repository->create(\$request->all());
+        empty(\$data) || \$data === null ?
+            \$response = \$this->errorResponse('Não foi possível criar uma instância de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
+            \$response = \$this->successResponse(new {$fileName}Resource(\$data));
 
-                empty(\$data) || \$data === null ?
-                    \$response = \$this->errorResponse('Não foi possível criar uma instância de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
-                    \$response = \$this->successResponse(new {$fileName}Resource(\$data));
+        return \$response;
+    }
 
-                return \$response;
-            }
+    public function callGetById(int \$id): JsonResponse
+    {
+        \$data = \$this->repository->getById(\$id);
 
-            public function callGetById(int \$id): JsonResponse
-            {
-                \$data = \$this->repository->getById(\$id);
+        empty(\$data) || \$data === null ?
+            \$response = \$this->errorResponse('Não foi possível trazer nada de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
+            \$response = \$this->successResponse(new {$fileName}Resource(\$data));
 
-                empty(\$data) || \$data === null ?
-                    \$response = \$this->errorResponse('Não foi possível trazer nada de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
-                    \$response = \$this->successResponse(new {$fileName}Resource(\$data));
+        return \$response;
+    }
 
-                return \$response;
-            }
+    public function callUpdate(int \$id, {$fileName}Request \$request): JsonResponse
+    {
+        \$data = \$this->repository->update(\$id, \$request->all());
 
-            public function callUpdate(int \$id, {$fileName}Request \$request): JsonResponse
-            {
-                \$data = \$this->repository->update(\$id, \$request->all());
+        empty(\$data) || \$data === null ?
+            \$response = \$this->errorResponse('Não foi possível trazer nada de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
+            \$response = \$this->successResponse(new {$fileName}Resource(\$data));
 
-                empty(\$data) || \$data === null ?
-                    \$response = \$this->errorResponse('Não foi possível trazer nada de {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
-                    \$response = \$this->successResponse(new {$fileName}Resource(\$data));
+        return \$response;
+    }
 
-                return \$response;
-            }
+    public function callDelete(int \$id): JsonResponse
+    {
+        \$data = \$this->repository->delete(\$id);
 
-            public function callDelete(int \$id): JsonResponse
-            {
-                \$data = \$this->repository->delete(\$id);
+        empty(\$data) || \$data === null ?
+            \$response = \$this->errorResponse('Não foi possível deletar {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
+            \$response = \$this->successResponse(\$data);
 
-                empty(\$data) || \$data === null ?
-                    \$response = \$this->errorResponse('Não foi possível deletar {$fileName}, está vazio ou nulo.', Response::HTTP_EXPECTATION_FAILED) :
-                    \$response = \$this->successResponse(\$data);
-
-                return \$response;
-            }
-        }
+        return \$response;
+    }
+}
 
         ";
     }
 
     public function routeTxt($fileName, $fileNameLower)
     {
-        return "
-        <?php
+        return "<?php
 
-        use App\Http\Controllers\\{$fileName}Controller;
-        use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\\{$fileName}Controller;
+use Illuminate\Support\Facades\Route;
 
-        Route::prefix('$fileNameLower')
-        ->name('$fileNameLower.')
-        ->group(function () {
-            Route::get('/', [{$fileName}Controller::class, 'callGetAll'])->name('get.all');
-            Route::post('/', [{$fileName}Controller::class, 'callCreate'])->name('create');
-            Route::get('/{ID}', [{$fileName}Controller::class, 'callGetById'])->name('get.by.id');
-            Route::put('/{ID}', [{$fileName}Controller::class, 'callUpdate'])->name('update');
-            Route::delete('/{ID}', [{$fileName}Controller::class, 'callDelete'])->name('delete');
-        });
+Route::prefix('$fileNameLower')
+->name('$fileNameLower.')
+->group(function () {
+    Route::get('/', [{$fileName}Controller::class, 'callGetAll'])->name('get.all');
+    Route::post('/', [{$fileName}Controller::class, 'callCreate'])->name('create');
+    Route::get('/{ID}', [{$fileName}Controller::class, 'callGetById'])->name('get.by.id');
+    Route::put('/{ID}', [{$fileName}Controller::class, 'callUpdate'])->name('update');
+    Route::delete('/{ID}', [{$fileName}Controller::class, 'callDelete'])->name('delete');
+});";
 
-        ";
     }
 }
 
